66350a6fe8fb1b71e451e3d4905eec71e9cbd7e3
yush-1018 <kokushibo1100@gmail.com>
2025-11-22 23:32:50 +0530

    FIRST WORK

---
+++ code/index.py
@@ -0,0 +1,20 @@
+# PROBABILITY, GUESSING GAME
+
+import random
+
+number = random.randint(1,100)
+
+attempts = 0
+max_attempts = 5
+while attempts < max_attempts:
+    guess = int(input("enter your guess (1-100):"))
+    attempts += 1
+    if guess < number:
+        print("too low!")
+    elif guess > number:
+        print("too high!")
+    else:
+        print(f"congratulations! you guessed the number {number} in {attempts} attempts.")
+        break
+else:
+    print(f"you used all attempts. the number was {number}. better luck next time!")
